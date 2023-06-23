585f2cddb34d950720c7b9054e9f7238a4b20f28
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-23 17:33:58 +0300

    feat: add rename

--- src/inputProcessing.js
+++ src/inputProcessing.js
@@ -4,6 +4,7 @@ import { listDirectories } from './nwd/listDirectories.js';
 import { changeDirectory } from './nwd/changeDirectory.js';
 import { readFile } from './bof/readFile.js';
 import { createFile } from './bof/createFile.js';
+import { renameFile } from './bof/rename.js';
 
 export async function inputProcessing(inputRow) {
     const [cmd, arg1, arg2] = inputRow.split(' ');
@@ -37,7 +38,18 @@ export async function inputProcessing(inputRow) {
                 console.log('Invalid input');
                 break;
         }
+    } else if (cmd && arg1 && arg2) {
+        switch (cmd) {
+            case 'rn':
+                await renameFile(arg1, arg2);
+                break;
+        
+            default:
+                console.log('Invalid input');
+                break;
+        }
     } else console.log('Invalid input');
+    
     
 
     console.log(`You are currently in ${process.cwd()}`);
