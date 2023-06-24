4b5d08c8a9154517d49cd223392f97e0037d69ae
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-24 14:49:22 +0300

    fix: format output for cpus

--- src/osi/osi.js
+++ src/osi/osi.js
@@ -6,7 +6,7 @@ export function osi(arg) {
             console.log(JSON.stringify(os.EOL));
             break;
         case '--cpus':
-            console.log(os.cpus());
+            cpusData(os.cpus());
             break;
         case '--homedir':
             console.log(os.homedir());
@@ -22,4 +22,14 @@ export function osi(arg) {
             console.log('Invalid input');
             break;
     }
+}
+
+function cpusData(data) {
+    const res = data.map(item => {
+        return {
+            model: item.model,
+            clock_rate: Math.round(item.speed/102,4) / 10 + ' GHz'
+        }
+    })
+    console.log(res);
 }
